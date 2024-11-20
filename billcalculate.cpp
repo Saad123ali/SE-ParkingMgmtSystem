3c6bf1a4e6cbb8197d6bab1e71ef65e8d5be740f
fax9 <hashmihamza894@gmail.com>
2024-11-20 19:59:31 +0500

    changes made to billcalculate class

--- billcalculate.cpp
+++ billcalculate.cpp
@@ -1,12 +1,31 @@
-
 class BillCalculate : public Vehicle
 {
-protected:
+public:
+    struct RateConfig
+    {
+        double carHourlyRate;
+        double carDailyRate;
+        double busHourlyRate;
+        double busDailyRate;
+        double bikeHourlyRate;
+        double bikeDailyRate;
+        double carHourlyFineRate;
+        double carDailyFineRate;
+        double busHourlyFineRate;
+        double busDailyFineRate;
+        double bikeHourlyFineRate;
+        double bikeDailyFineRate;
+    };
+
+private:
     int token;
     double amount;
     double extraAmount;
     bool paymentSuccess;
     double fine;
+    unordered_map<int, bool> generatedTokens; // Map to store unique tokens
+
+protected:
     double totalAmount;
 
 public:
@@ -22,14 +41,17 @@ public:
         tm *ltm = localtime(&now);
         return to_string(1900 + ltm->tm_year) + "-" + to_string(ltm->tm_mon + 1) + "-" + to_string(ltm->tm_mday) + " " + to_string(ltm->tm_hour) + ":" + to_string(ltm->tm_min) + ":" + to_string(ltm->tm_sec);
     }
-    
+
     void GenerateToken()
     {
         srand(time(0)); // Seed for random number generator
-        token = rand() % 90000 + 100000;
+        do
+        {
+            token = rand() % 90000 + 100000; // Generate a token in the range 100000 to 999999
+        } while (generatedTokens.find(token) != generatedTokens.end()); // Check for duplicates
+        generatedTokens[token] = true; // Store the newly generated token
     }
-
-    int getToken()
+    int getToken() const
     {
         return token;
     }
@@ -39,38 +61,79 @@ public:
         return totalAmount;
     }
 
-    void calculateBill(string vehicleType)
+    double calculateOverstayTime(time_t entryTime, int allocatedHours, int allocatedDays)
+    {
+        // Get the current time
+        time_t currentTime;
+        time(&currentTime);
+
+        // Calculate total parked time in seconds
+        double secondsParked = difftime(currentTime, entryTime);
+
+        // Convert parked time to hours and days
+        double hoursParked = ceil(secondsParked / 3600.0);
+        double daysParked = ceil(secondsParked / (24 * 3600.0));
+
+        // Calculate overstay time in hours and days
+        double overstayHours = hoursParked - allocatedHours;
+        double overstayDays = daysParked - allocatedDays;
+
+        // Ensure overstay time is not negative
+        if (overstayHours < 0)
+        {
+            overstayHours = 0;
+        }
+        if (overstayDays < 0)
+        {
+            overstayDays = 0;
+        }
+
+        // Return the maximum overstay time
+        return max(overstayHours, overstayDays);
+    }
+
+    void calculateBill(string vehicleType, const RateConfig &rates)
     {
         time_t currentTime;
         time(&currentTime);
-        double seconds = difftime(currentTime, getParkTime());
-        double hoursParked = ceil(seconds / 3600.0);
-        double daysParked = ceil(seconds / (24 * 3600.0));
+        double overstayHours = 0;
+        double overstayDays = 0;
+
+        if (parkingType == "1")
+        {
+            // Calculate overstay for hourly parking
+            overstayHours = calculateOverstayTime(getParkTime(), hours, 0);
+        }
+        else if (parkingType == "2")
+        {
+            // Calculate overstay for daily parking
+            overstayDays = calculateOverstayTime(getParkTime(), 0, days);
+        }
 
         if (vehicleType == "Car")
         {
             if (parkingType == "2")
             {
-                if (daysParked > days)
+                if (overstayDays > 0)
                 {
-                    fine = (daysParked - days) * 50;
-                    totalAmount = daysParked * 70 + fine;
+                    fine = overstayDays * rates.carDailyFineRate;
+                    totalAmount = (days * rates.carDailyRate) + fine;
                 }
                 else
                 {
-                    totalAmount = days * 70;
+                    totalAmount = days * rates.carDailyRate;
                 }
             }
             else if (parkingType == "1")
             {
-                if (hoursParked > hours)
+                if (overstayHours > 0)
                 {
-                    fine = (hoursParked - hours) * 50;
-                    totalAmount = hoursParked * 70 + fine;
+                    fine = overstayHours * rates.carHourlyFineRate;
+                    totalAmount = (hours * rates.carHourlyRate) + fine;
                 }
                 else
                 {
-                    totalAmount = hours * 70;
+                    totalAmount = hours * rates.carHourlyRate;
                 }
             }
         }
@@ -78,26 +141,26 @@ public:
         {
             if (parkingType == "2")
             {
-                if (daysParked > days)
+                if (overstayDays > 0)
                 {
-                    fine = (daysParked - days) * 70;
-                    totalAmount = daysParked * 100 + fine;
+                    fine = overstayDays * rates.busDailyFineRate;
+                    totalAmount = (days * rates.busDailyRate) + fine;
                 }
                 else
                 {
-                    totalAmount = days * 100;
+                    totalAmount = days * rates.busDailyRate;
                 }
             }
             else if (parkingType == "1")
             {
-                if (hoursParked > hours)
+                if (overstayHours > 0)
                 {
-                    fine = (hoursParked - hours) * 70;
-                    totalAmount = hoursParked * 100 + fine;
+                    fine = overstayHours * rates.busHourlyFineRate;
+                    totalAmount = (hours * rates.busHourlyRate) + fine;
                 }
                 else
                 {
-                    totalAmount = hours * 100;
+                    totalAmount = hours * rates.busHourlyRate;
                 }
             }
         }
@@ -105,26 +168,26 @@ public:
         {
             if (parkingType == "2")
             {
-                if (daysParked > days)
+                if (overstayDays > 0)
                 {
-                    fine = (daysParked - days) * 30;
-                    totalAmount = daysParked * 50 + fine;
+                    fine = overstayDays * rates.bikeDailyFineRate;
+                    totalAmount = (days * rates.bikeDailyRate) + fine;
                 }
                 else
                 {
-                    totalAmount = days * 50;
+                    totalAmount = days * rates.bikeDailyRate;
                 }
             }
             else if (parkingType == "1")
             {
-                if (hoursParked > hours)
+                if (overstayHours > 0)
                 {
-                    fine = (hoursParked - hours) * 30;
-                    totalAmount = hoursParked * 50 + fine;
+                    fine = overstayHours * rates.bikeHourlyFineRate;
+                    totalAmount = (hours * rates.bikeHourlyRate) + fine;
                 }
                 else
                 {
-                    totalAmount = hours * 50;
+                    totalAmount = hours * rates.bikeHourlyRate;
                 }
             }
         }
@@ -135,19 +198,45 @@ public:
         system("CLS");
         cout << "\n*************** Parking Bill ***************\n";
         if (isParked)
+        {
             cout << "Time of Entry     : " << GetCurrentTime() << "\n";
+            cout << "Phone Number      : " << cellNo << "\n";
+            cout << "Plate Number      : " << plateNo << "\n";
+            if (parkingType == "2")
+                cout << "Days Parked       : " << days << " Days" << "\n";
+            else if (parkingType == "1")
+                cout << "Hours Parked      : " << hours << " Hours" << "\n";
+            cout << "Token Number      : " << token << "\n";
+            cout << "Amount            : Rs." << totalAmount << "\n"; // Show amount when parked
+        }
         else
+        {
             cout << "Exit Time         : " << GetCurrentTime() << "\n";
-        cout << "Phone Number      : " << cellNo << "\n";
-
-        cout << "Plate Number      : " << plateNo << "\n";
-        if (parkingType == "2")
-            cout << "Days Parked       : " << days << " Days" << "\n";
-        else if (parkingType == "1")
-            cout << "Hours Parked      : " << hours << " Hours" << "\n";
-        cout << "Token Number      : " << token << "\n";
-        cout << "Fine Amount       : Rs." << fine << "\n";
-        cout << "Total Amount      : Rs." << totalAmount << "\n";
+            cout << "Phone Number      : " << cellNo << "\n";
+            cout << "Plate Number      : " << plateNo << "\n";
+            if (parkingType == "2")
+            {
+                cout << "Days Parked       : " << days << " Days" << "\n";
+                double overstayDays = ceil(difftime(time(0), getParkTime()) / (24 * 3600.0)) - days;
+                if (overstayDays > 0)
+                {
+                    cout << "Overstay Days     : " << overstayDays << " Days" << "\n";
+                    cout << "Fine Amount       : Rs." << fine << "\n";
+                }
+            }
+            else if (parkingType == "1")
+            {
+                cout << "Hours Parked      : " << hours << " Hours" << "\n";
+                double overstayHours = ceil(difftime(time(0), getParkTime()) / 3600.0) - hours;
+                if (overstayHours > 0)
+                {
+                    cout << "Overstay Hours    : " << overstayHours << " Hours" << "\n";
+                    cout << "Fine Amount       : Rs." << fine << "\n";
+                }
+            }
+            cout << "Token Number      : " << token << "\n";
+            cout << "Total Amount      : Rs." << totalAmount << "\n"; // Show total amount when unparked
+        }
         cout << "********************************************\n";
     }
 
@@ -187,3 +276,4 @@ public:
         }
     }
 };
+
